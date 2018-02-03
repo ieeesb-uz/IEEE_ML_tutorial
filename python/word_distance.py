eea1fe32d5ff555fa4cd6a485db849db3baf71b2
Jorge Cáncer Gil <646122@unizar.es>
2018-02-03 10:56:48 +0100

    Create word_distance.py

---
+++ python/word_distance.py
@@ -0,0 +1,26 @@
+@mfunction("distance")
+def word_distance(word1=None, word2=None, model=None):
+    # Shows the L2 distance between word1 and word2 in the word_embedding_weights.
+    # Inputs:
+    #   word1: The first word as a string.
+    #   word2: The second word as a string.
+    #   model: Model returned by the training script.
+    # Example usage:
+    #   word_distance('school', 'university', model);
+
+    word_embedding_weights = model.word_embedding_weights
+    vocab = model.vocab
+    id1 = strmatch(word1, vocab, mstring('exact'))
+    id2 = strmatch(word2, vocab, mstring('exact'))
+    if not any(id1):
+        fprintf(1, mstring('Word \'%s\\\' not in vocabulary.\\n'), word1)
+        return
+        end
+        if not any(id2):
+            fprintf(1, mstring('Word \'%s\\\' not in vocabulary.\\n'), word2)
+            return
+            end
+            word_rep1 = word_embedding_weights(id1, mslice[:])
+            word_rep2 = word_embedding_weights(id2, mslice[:])
+            diff = word_rep1 - word_rep2
+            distance = sqrt(sum(diff *elmul* diff))
